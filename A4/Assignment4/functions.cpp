ab7f53ec87096e7e2c00a8faa6b00caaa8062fcc
squidgrrl <kdunlap@mymail.mines.edu>
2017-10-10 13:40:18 -0600

    started assignment4

---
+++ A4/Assignment4/functions.cpp
@@ -0,0 +1,9 @@
+/* CSCI 261
+ * Assignment4 Functions
+ * Author: Krista Dunlap
+*/
+
+
+//Printing out current solved puzzle
+
+//Returning t/f if letter has been guessed
---
+++ A4/Assignment4/main.cpp
@@ -0,0 +1,26 @@
+/* CSCI 261 Assignment 3: Wheel of Fortune!
+ *
+ * Author: Krista Dunlap
+ *
+ * A game of hangman
+ */
+
+//Inlcluding libraries needed
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Declaring/intializing constants and arrays
+	const int SIZE = 6;
+	const char theWord[SIZE] = {'S','Q','U','I','D','S'};
+	char userWord[SIZE] = {'_','_','_','_','_','_'};
+	char previousGuesses[12];
+	int guessesLeft = 12;
+
+	cout << "Welcome to Wheel of Fortune!" << endl;
+
+	while(guessesLeft > 0 && )
+
+	return 0;
+}
