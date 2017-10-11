a323f2372e9df382d3a30a6dad9e9535a9e609ac
squidgrrl <kdunlap@mymail.mines.edu>
2017-10-11 13:55:36 -0600

    started all A5 files

---
+++ A5/Assignment5/main.cpp
@@ -0,0 +1,15 @@
+/* CSCI 261 Assignment 5: Blackjack
+ *
+ * Author: Krista Dunlap
+ *
+ * A game of blackjack
+ */
+
+//Inlcluding libraries needed
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
---
+++ A5/Lab5B/main.cpp
@@ -0,0 +1,15 @@
+/* CSCI 261 Lab 5B: Vector Usage
+ *
+ * Author: Krista Dunlap
+ *
+ * FILL
+ */
+
+//Inlcluding libraries needed
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
