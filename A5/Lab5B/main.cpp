c3134da7044715d2d6cc41c36b1bbb427045feac
squidgrrl <kdunlap@mymail.mines.edu>
2017-10-19 19:46:54 -0600

    Finished Lab5B

--- A5/Lab5B/main.cpp
+++ A5/Lab5B/main.cpp
@@ -5,21 +5,60 @@
  * Making Lab4B more flexible using vectors
  */
 
+
 //Inlcluding libraries needed
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int findMax(vector<int> userVector) {
+
+	int vectorMax;
+
+	vectorMax = userVector[0];
+
+	for(int i = 0; i < userVector.size(); ++i) {
+
+		if(userVector[i] > vectorMax) {
+
+			vectorMax = userVector[i];
+
+		}
+
+	}
+
+	return vectorMax;
+
+}
+
+int findMin(vector<int> userVector) {
+
+	int vectorMin;
+
+	vectorMin = userVector[0];
+
+	for(int i = 0; i < userVector.size(); ++i) {
+
+		if(userVector[i] < vectorMin) {
+
+			vectorMin = userVector[i];
+
+		}
+
+	}
+
+	return vectorMin;
+
+}
 
 int main() {
 
 	//Initial Hello Message
 	cout << "Hello and welcome! Please enter as many non-negative numbers as you would like. Enter -1 to stop." << endl;
 
-	//Declaring/intializing userVector, currentNum, minNum, maxNum
+	//Declaring/intializing userVector, currentNum
 	vector<int> userVector;
 	int currentNum = -3;
-	int minNum;
-	int maxNum;
 
 	//Loop to enter numbers
 	while(currentNum != -1) {
@@ -49,14 +88,23 @@ int main() {
 	//Prints how many numbers were entered
 	cout << "Done! You entered " << userVector.size() << " numbers." << endl;
 
-	//Using a loop to figure out the smallest number
+	cout << "The numbers are: ";
+
 	for(int i = 0; i < userVector.size(); ++i) {
 
-		minNum = userVector.at(0);
-
-		
+		cout << userVector[i] << " ";
 
 	}
+
+	cout << endl << "The smallest number is: " << findMin(userVector) << endl;
+
+	cout << "The largest number is: " << findMax(userVector) << endl;
+
+	cout << "The first number is: " << userVector[0] << endl;
+
+	cout << "The last number is: " << userVector.back() << endl;
+
+	cout << "Have a nice day!" << endl;	
 
 	return 0;
 }
