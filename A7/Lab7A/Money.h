731a7723b4aa89f0ec6dd4bb19923428e4777fbf
squidgrrl <kdunlap@mymail.mines.edu>
2017-11-08 10:03:11 -0700

    Started A7 & Lab7A

---
+++ A7/Lab7A/Money.h
@@ -0,0 +1,19 @@
+/* CSCI261 Lab7A: Money Class
+ *
+ * Description: Declaration file for Money Class
+ *
+ * Author: Krista Dunlap
+ *
+ */
+
+#pragma once
+
+class Money {
+
+	public:
+		int dollars;
+		int cents;
+		Money();
+		Money(int, int);
+
+};
