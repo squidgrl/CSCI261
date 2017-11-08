2ef372147325d7401db7f7ac96b795ead7ee0c48
squidgrrl <kdunlap@mymail.mines.edu>
2017-11-08 10:03:26 -0700

    Exam Practice

--- Exam/main.cpp
+++ Exam/main.cpp
@@ -1,16 +1,64 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 
-	bool isPrime = 0;
+	/*double numbers[2][2] = {1,2,3,4};
 
-	if(isPrime) {
-		cout << "yeah";
+	double sums[2] = {0};
+
+	for(int i = 0; i < 2; ++i) {
+
+		for(int j = 0; j < 2; ++j) {
+
+			sums[i] += numbers[i][j];
+
+		}
+
 	}
-	else {
-		cout << "nah";
+
+	for(int i = 0; i < 2; ++i) {
+
+		cout << sums[i] << " ";
+
 	}
+
+	cout << endl;*/
+
+	/*bool arr[4] = {true};
+
+	for(int i = 0; i < 4; ++i) {
+
+		cout << arr[i] << " ";
+
+	}
+
+	cout << endl;*/
+
+	int arr[2][3] = {1,2,3,1,2,3};
+
+	for(int i = 0; i < 2; ++i) {
+
+		for(int j = 0; j < 3; ++j) {
+
+			cout << arr[i][j] << " ";
+
+		}
+	}
+
+	cout << endl;
+
+	/*string s = "stop rolling";
+
+	cout << s.size() << endl;
+
+	s.replace(4,1,"X");
+
+	cout << s << endl;
+
+	cout << s.at(5) << endl;*/
+
 	return 0;
 
 }
